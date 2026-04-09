881562a5f5f0a11ef660099e3ef898ccdd53fb43
kazuminf28 <kazumin.f28@gmail.com>
2026-04-09 17:37:18 +0900

    fix: apply gitignore

---
+++ 3DActionGame/Assets/Script/PlayerController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator anim;
+    public float PlayerSpeed;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+       Move();
+    }
+
+#region MoveControls
+    void Move()
+    {
+        //カメラの向き基準の正面方向ベクトル
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+        Vector3 x = Camera.main.transform.right * Input.GetAxis("Horizontal") * PlayerSpeed;
+        Vector3 z = cameraForward * Input.GetAxis("Vertical") * PlayerSpeed;
+        Vector3 move = x + z;
+        if (move.magnitude > 0)
+        {
+            anim.SetBool("IsRun", true);
+
+            move = move.normalized;
+            // 向きを入力方向に
+            Quaternion targetRotation = Quaternion.LookRotation(move);
+            
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.15f);
+            // 実際に移動
+            transform.position += move * Time.deltaTime * PlayerSpeed;
+        } else
+        {
+            anim.SetBool("IsRun", false);
+        }
+    }
+#endregion
+
+    
+}
