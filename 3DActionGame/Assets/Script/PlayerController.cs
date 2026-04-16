55adcae5f758c541ac68dbe5ae6289e3915db46c
kazuminf28 <kazumin.f28@gmail.com>
2026-04-16 16:57:02 +0900

    Add AttackDestory
    敵を削除する機構と剣の当たり判定の実装をした

--- 3DActionGame/Assets/Script/PlayerController.cs
+++ 3DActionGame/Assets/Script/PlayerController.cs
@@ -10,6 +10,12 @@ public class PlayerController : MonoBehaviour
     [Header("ダッシュ時のスピード")]
     public float SprintSpeed;
 
+    public Collider WeaponCollider;
+    bool switchMove = true;
+    [Header("DashAttack")]
+    Vector3 dashVelocity;
+    bool isDashAttacking = false;
+
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -20,11 +26,17 @@ public class PlayerController : MonoBehaviour
     {
        Move();
        Attack();
+       if (isDashAttacking)
+        {
+            transform.position += dashVelocity * Time.deltaTime;
+            dashVelocity *= 0.99f; //減速
+        }
     }
 
 #region MoveControls
     void Move()
     {
+        if(!switchMove) return;
         //カメラの向き基準の正面方向ベクトル
         Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
 
@@ -59,17 +71,50 @@ public class PlayerController : MonoBehaviour
         bool Sprint = Input.GetKey(KeyCode.LeftShift);
         if (Input.GetMouseButtonDown(0))
         {
+            switchMove = false;
             if (Sprint && anim.GetBool("IsRun"))
             {
                 anim.SetTrigger("IsDushAttack");
-            } else if (combo.canCombo)
-            {
-                anim.SetTrigger("IsAttack");
+                anim.SetBool("CanCombo", combo.canCombo);
+            // } else if (combo.canCombo)
+            // {
+            //     anim.SetTrigger("IsAttack");
             } else
             {
                 anim.SetTrigger("IsAttack");
             }
         }
     }
+
+    public void StartDashAttack()
+    {
+        isDashAttacking = true;
+        dashVelocity = transform.forward * 10f;
+    }
+
+    public void EndDashAttack()
+    {
+        isDashAttacking = false;
+        dashVelocity = Vector3.zero;
+    }
 #endregion 
+
+#region Weapon Contoroller
+    void OnWeapon()
+    {
+        WeaponCollider.enabled = true;
+    }
+
+    void OffWeapon()
+    {
+        WeaponCollider.enabled = false;
+    }
+#endregion 
+
+#region 
+    public void CanMove()
+    {
+        switchMove = true;
+    }
+#endregion
 }
