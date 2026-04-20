89293ca17c2403d9de001cc36d326f44a19d4568
kazuminf28 <kazumin.f28@gmail.com>
2026-04-20 17:12:20 +0900

    Change Move
    移動処理のちょっとした改善

--- 3DActionGame/Assets/Script/EnemyController.cs
+++ 3DActionGame/Assets/Script/EnemyController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Security.Cryptography;
 using UnityEngine;
 
@@ -51,7 +52,7 @@ public class EnemyController : MonoBehaviour
             transform.rotation = Quaternion.LookRotation(direction);
             transform.position = Vector3.MoveTowards(
             transform.position,
-            player.transform.position,
+            new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z),
             Time.deltaTime * MoveSpeed
             );
             anim.SetBool("IsRun", true);
@@ -68,9 +69,9 @@ public class EnemyController : MonoBehaviour
                     SetRandomDirection();
                 }
                 return;
-            } else
-            {
-                Movetimer -= Time.deltaTime;
+            }
+            Movetimer -= Time.deltaTime;
+            if(Movetimer <=0f){
                 if(Random.value < 0.3f)// 30%
                 {
                     IsWait = true;
@@ -79,11 +80,11 @@ public class EnemyController : MonoBehaviour
                 }
                 SetRandomDirection();
                 Movetimer = Random.Range(2f, 4f);
-                Quaternion targetRotation = Quaternion.LookRotation(RandomDirection);
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2f);
-                transform.position += RandomDirection * MoveSpeed * 0.5f * Time.deltaTime;
-                anim.SetBool("IsRun", true);
             }
+            Quaternion targetRotation = Quaternion.LookRotation(RandomDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2f);
+            transform.position += RandomDirection * MoveSpeed * 0.5f * Time.deltaTime;
+            anim.SetBool("IsRun", true);
         }
     }
 
@@ -94,11 +95,20 @@ public class EnemyController : MonoBehaviour
         RandomDirection = new Vector3(Mathf.Cos(rad), 0, Mathf.Sin(rad));
     }
 
+    IEnumerator  HitStop()
+    {
+        float originalSpeed = MoveSpeed;
+        MoveSpeed = 0f;
+        yield return new WaitForSeconds(0.2f);
+        MoveSpeed = originalSpeed;
+    }
+
     public void HitDamage(float damage)
     {
-        currentHP -= damage - Defense;
+        float finaldamage = Mathf.Max(damage - Defense, 1f);
+        currentHP -= finaldamage;
         // anim.SetTrigger("IsHit");
-
+        StartCoroutine(HitStop());
         if (currentHP <= 0)
         {
             Die();
