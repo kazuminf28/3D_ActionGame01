01bcb6f0653a8dbd7a05c0fb0aacdbb8dd4435ce
kazuminf28 <kazumin.f28@gmail.com>
2026-05-24 11:11:39 +0900

    add timer
    timer追加

---
+++ 3DActionGame/Assets/Script/Timer.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+
+    private float timer;
+    private bool isStop = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isStop) return;
+        timer += Time.deltaTime;
+
+        int minutes = Mathf.FloorToInt(timer / 60);
+        int seconds = Mathf.FloorToInt(timer % 60);
+        int milliseconds = Mathf.FloorToInt((timer * 100) % 100);
+
+        timerText.text = minutes.ToString("00") + ":" +
+                        seconds.ToString("00") + ":" +
+                        milliseconds.ToString("00");
+    }
+
+    public void StopTimer()
+    {
+        isStop = true;
+    }
+
+    public void StartTimer()
+    {
+        isStop = false;
+    }
+}
